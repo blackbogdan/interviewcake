1d5b2da0e69c84563786387582ec415ee1283f82
bkapusta <bkapusta@bitglass.com>
2017-03-22 20:34:03 -0700

    added to gitignore and to task8 binary tree

--- task8_binary_tree_superbalanced.py
+++ task8_binary_tree_superbalanced.py
@@ -1,1 +1,18 @@
 __author__ = 'bkapusta'
+# Binary tree explained:
+# https://www.youtube.com/watch?v=YlgPi75hIBc&index=7&list=PLj8W7XIvO93qsmdxbaDpIvM1KCyNO1K_c
+class BinaryTreeNode:
+    def __init__(self, value):
+        self.value = value
+        self.left = left
+        self.right = right
+
+    def insert_left(self):
+        self.left = BinaryTreeNode.value
+        return self.left
+
+    def insert_right(self):
+        self.right = BinaryTreeNode.value
+        return self.right
+
+
