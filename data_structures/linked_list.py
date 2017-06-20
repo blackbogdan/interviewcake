1ffffdc5f18f6ae84555cfe0c25a4a6abf5a4279
bkapusta <bkapusta@bitglass.com>
2017-06-20 09:58:36 -0700

    adding linked list implementation

--- data_structures/linked_list.py
+++ data_structures/linked_list.py
@@ -1,1 +1,71 @@
 __author__ = 'bkapusta'
+
+
+class Node(object):
+
+    def __init__(self, d, n = None):
+        # d - piece of data
+        # n - next node
+        self.data = d
+        self.next_node = n
+
+    def get_next(self):
+        return self.next_node
+
+    def set_next(self, n):
+        self.next_node = n
+
+    def get_data(self):
+        return self.data
+
+    def set_data(self, d):
+        self.data = d
+
+
+class LinkedList(object):
+
+    def __init__(self, r = None):
+        self.root = r
+        self.size = 0
+
+    def get_size(self):
+        return self.size
+
+    def add(self, d):
+        new_node = Node(d, self.root)
+        self.root = new_node
+        self.size +=1
+
+    def remove(self, d):
+        this_node = self.root
+        prev_node = None
+        while this_node:
+            if this_node.get_data() == d:
+                if prev_node:
+                    prev_node.set_next(this_node.get_next())
+                else:
+                    self.root = this_node
+                self.size -=1
+                return True  #data was removed
+            else:
+                prev_node = this_node
+                this_node = this_node.get_next()
+        return False # we did no find the item
+    def find(self, d):
+
+        this_node = self.root
+        while this_node:
+            # print "lkj"
+            if this_node.get_data() == d:
+                return d
+            else:
+                this_node = this_node.get_next()
+        return None
+
+My_list = LinkedList()
+My_list.add(5)
+My_list.add(8)
+My_list.add(12)
+My_list.add(8)
+print My_list.remove(12)
+print My_list.find(5)
