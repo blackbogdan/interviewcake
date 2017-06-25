78279ba6b6105fd9d4316f4e5cf9d595c427a5a2
bkapusta <bkapusta@bitglass.com>
2017-06-25 13:36:02 -0700

    changed implememntation of linked list

--- data_structures/linked_list.py
+++ data_structures/linked_list.py
@@ -1,71 +1,63 @@
-__author__ = 'bkapusta'
-
-
-class Node(object):
-
-    def __init__(self, d, n = None):
-        # d - piece of data
-        # n - next node
+class Node:
+    def __init__(self, d, n=None):
         self.data = d
         self.next_node = n
 
-    def get_next(self):
-        return self.next_node
-
-    def set_next(self, n):
-        self.next_node = n
-
-    def get_data(self):
-        return self.data
-
-    def set_data(self, d):
-        self.data = d
-
-
-class LinkedList(object):
-
+class LinkedList:
     def __init__(self, r = None):
         self.root = r
         self.size = 0
 
-    def get_size(self):
-        return self.size
-
     def add(self, d):
         new_node = Node(d, self.root)
         self.root = new_node
-        self.size +=1
+        self.size += 1
 
-    def remove(self, d):
-        this_node = self.root
-        prev_node = None
-        while this_node:
-            if this_node.get_data() == d:
-                if prev_node:
-                    prev_node.set_next(this_node.get_next())
-                else:
-                    self.root = this_node
-                self.size -=1
-                return True  #data was removed
-            else:
-                prev_node = this_node
-                this_node = this_node.get_next()
-        return False # we did no find the item
     def find(self, d):
 
         this_node = self.root
         while this_node:
-            # print "lkj"
-            if this_node.get_data() == d:
+            if this_node.data == d:
                 return d
             else:
-                this_node = this_node.get_next()
-        return None
+                this_node = this_node.next_node
+        return False
 
-My_list = LinkedList()
-My_list.add(5)
-My_list.add(8)
-My_list.add(12)
-My_list.add(8)
-print My_list.remove(12)
-print My_list.find(5)
+    def remove(self, d):
+        # set the root node as beginning of search
+        this_node = self.root
+        prev_node = None
+        # loop through the linked list until this_node == None
+        # that will happen when we reached the end fo the linked list
+        while this_node:
+            # if we found the item
+            if this_node.data == d:
+                # we found the item and it's in the middle of the list
+                # (not the first (self.root item). ==> it has previous
+                # node which is not none
+                if prev_node:
+                    # print "setting pointers"
+                    prev_node.next_node = this_node.next_node
+                else:
+                    self.root = this_node
+                self.size -= 1
+                return True #data was removed
+            else:
+                prev_node = this_node
+                this_node = this_node.next_node
+        return False #we did not find the item in list
+    def print_all(self):
+        this_node = self.root
+        while this_node:
+            print this_node.data
+            this_node = this_node.next_node
+if __name__ == "__main__":
+    My_list = LinkedList()
+    My_list.add(4)
+    My_list.add(4)
+    My_list.add(5)
+    My_list.add(1)
+    My_list.add(12)
+    My_list.remove(5)
+    print "size of the list: ", My_list.size
+    print My_list.print_all()
